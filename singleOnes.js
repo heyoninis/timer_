93f9c9093a8be4f9d29e3725b81ad8d2390d6c33
Anne Souza <131973049+heyoninis@users.noreply.github.com>
2023-11-07 18:11:06 -0300

    0.1.1
    Consegui fazer funcionar o negocio de salvar a variavel
    Falta fazer funcionar com os 00 e afins
    
    fazer salvar valores
    fazer se input ja tiver valor, iniciar logo do Saveinfo, caso não

--- singleOnes.js
+++ singleOnes.js
@@ -1,31 +1,32 @@
 // Defina as variáveis globais aqui, mas não leia seus valores ainda, só chama
 let minutesInput, secondsInput;
-const countdownElement = document.getElementById("countdown");
-const buttonStart = document.getElementById("startButton");
-const buttonSave = document.getElementById("saveInfo");
-const buttonBack = document.getElementById("backButton");
-const buttonRedo = document.getElementById("redoButton");
 
-// var timeM = document.getElementById("minutesInput").value;
-// let timeM = minutesInput;
+const countdownElement = document.getElementById("countdown"); // é a div que mostra o tempo
+const showNameElement = document.getElementById("showName"); // Onde mostra o nome do timer
+const buttonStart = document.getElementById("startButton"); // botão para iniciar timer
+const buttonSave = document.getElementById("saveInfo"); // botão que salva as infos do input
+const buttonBack = document.getElementById("backButton"); // botão para retornar a tela de info salva
+const buttonRedo = document.getElementById("redoButton"); // refazer funcionar o timer
 
-// var timeM = document.getElementById("secondsInput").value;
-// let timeS = secondsInput;
 
-const form = document.getElementById("timer");
-const inputTimer = document.getElementsByClassName("inpuTimer");
+const form = document.getElementById("timer"); // div onde ta os inputs de tempo
 
+// Setando o style dos botões ao iniciar a página
+
+showNameElement.style.display = "none";
 buttonRedo.style.display = "none";
 buttonBack.style.display = "none";
 buttonStart.style.display = "none";
 
-
 function saveInfo() {
     // Leia os valores dos elementos de entrada dentro da função
+    nameInput = document.getElementById("nameInput").value;
     minutesInput = document.getElementById("minutesInput").value;
+    minutesSave = document.getElementById("minutesInput").value;
     secondsInput = document.getElementById("secondsInput").value;
+    secondsSave = document.getElementById("secondsInput").value;
 
-    if ( !minutesInput && !secondsInput) {
+    if (!minutesInput && !secondsInput) {
         alert("Por favor insira valores para minutos e/ou segundos");
     } else {
         minutesInput = minutesInput || "00";
@@ -41,17 +42,16 @@ function saveInfo() {
 
         form.style.display = "none";
         buttonStart.style.display = "block";
+        showNameElement.style.display = "block";
+
 
         countdownElement.innerHTML = minutesInput + ":" + secondsInput;
+        showNameElement.innerHTML = nameInput;
     }
 }
 
 
-
-
 // Ver se vai funcionar cpom tudo
-
-
 document.addEventListener("DOMContentLoaded", function () {
 
     // Aqui está chamando os itens lá do html
@@ -61,11 +61,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const countdownElement = document.getElementById("countdown");
 
     // Essa variavel está indefinida pois ela é usada para saber se tem algo sendo rodao, ou seja, se tiver já uma contagem rodando o timer vai ter um valor, caso cotrárioele não vai ter. (Não entendi muito bem, mas sem isso não vai funcionar direito)
-
     let timer;
 
     // Onclick lá do html, ta adicionando um evento
-    // Como no botão lá não tem um onclick, ele é adicionado aquiusando o addEventListener
+    // Como no botão lá não tem um onclick, ele é adicionado aqui usando o addEventListener
     startButton.addEventListener("click", function () {
 
         buttonStart.style.display = "none";
@@ -103,15 +102,49 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // aqui mostra tudo isso no html
             countdownElement.innerHTML = `${minutes}m ${seconds}s`;
+            showNameElement.style.display = "block";
+
 
             if (remainingTime === 0) {
+
                 // Se acabar mostra a msn e limpa o timer
                 buttonStart.style.display = "block";
+                showNameElement.style.display = "block";
 
-                countdownElement.innerHTML = minutesInput + ":" + secondsInput;
+
+                // minutesSave = minutesSave || "00";
+                // secondsSave = secondsSave || "00";
+
+                while (secondsSave.length === 1) {
+
+                    if (secondsSave < 10 && secondsSave !== 0) {
+                        secondsSave = "0" + secondsSave;
+                    }
+
+                    if (secondsSave === null) {
+                        secondsSave === "00"
+                    }
+
+                }
+
+                while (minutesSave.length === 1) {
+
+                    if (minutesSave < 10 && minutesSave !== 0) {
+                        minutesSave = "0" + minutesSave;
+                    }
+
+
+                    if (minutesSave === 0) {
+                        minutesSave === "00"
+                    }
+                }
+
+                countdownElement.innerHTML = minutesSave + ":" + secondsSave;
+
 
             } else {
 
+                showNameElement.style.display = "block";
                 buttonStart.style.display = "none";
 
                 // Se não, vai tirando um do valor do remainin time
